2ef4c25f5d0aa476e6dfafce6210b30510bb2344
badradine <bodourali235@gmail.com>
2023-09-19 02:58:20 +0300

    ajouter file

---
+++ badour2.py
@@ -0,0 +1,19 @@
+num = 10
+
+if num > 0:
+    print("Number is positive")
+elif num < 0:
+    print("Number is negative")
+else:
+    print("Number is zero")
+
+# For loop
+for i in range(1, 6):
+    print("For loop:", i)
+
+# While loop
+count = 1
+while count <= 5:
+    print("While loop:", count)
+    count += 1
+
---
+++ badour3.py
@@ -0,0 +1,19 @@
+numbers = [1, 2, 3, 4, 5]
+
+# Accessing elements
+print("Element at index 2:", numbers[2])
+
+# Modifying elements
+numbers[3] = 10
+
+# Iterating through the list
+for num in numbers:
+    print("Element:", num)
+
+def greet(name):
+    return "Hello, " + name + "!"
+
+# Function call
+greeting = greet("Alice")
+print(greeting)
+
