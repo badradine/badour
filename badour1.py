aa67144525155ce282595a2a2ef039eaaed8288a
badradine <bodourali235@gmail.com>
2023-09-19 02:56:18 +0300

    ajouter file

---
+++ badour1.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+
+num = 10  # Integer
+decimal = 5.5  # Float
+text = "Hello, Python!"  # String
+is_python_fun = True  # Boolean
+
+print("Number:", num)
+print("Decimal:", decimal)
+print("Text:", text)
+print("Is Python fun?", is_python_fun)
+
